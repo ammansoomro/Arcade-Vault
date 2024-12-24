66d4ca73199d738bb6525ca265d1630863dd881d
muhammad-amman <amman.soomro.as@gmail.com>
2024-12-25 00:28:37 +0500

    Setup server.js to start the backend and connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js"
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 // App Config
 const app = express();
-const port = 4007;
+const PORT = process.env.PORT || 4007;
 
 // Middles Wares
 app.use(express.json());
 app.use(cors());
 
+// Database Connection
+connectDB();
+
+
 app.get("/", (req, res) => {
   res.send("Api Working");
 });
 
-app.listen(port, () => {
-  console.log(`Server Started on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server Started on http://localhost:${PORT}`);
 });
