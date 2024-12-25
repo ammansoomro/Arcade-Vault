01bc3f6547c85ccc0264ad79b447f7bc451d80e5
muhammad-amman <amman.soomro.as@gmail.com>
2024-12-25 11:01:58 +0500

    Updated Logo and Added Browser Router

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,5 +1,10 @@
 import logo from "./logo.svg";
-
+import add_icon from "./add_icon.svg";
+import order_icon from "./order_icon.svg";
+import list_icon from "./list_icon.svg";
 export const assets = {
   logo,
+  add_icon,
+  list_icon,
+  order_icon,
 };
