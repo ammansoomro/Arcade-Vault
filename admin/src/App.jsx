cd9bcb8277ee7332d535069b64f37479d1dd6d0a
muhammad-amman <amman.soomro.as@gmail.com>
2024-12-25 11:02:34 +0500

    Imported Navbar and Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar/navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+      <div className="app-content">
+        <Sidebar></Sidebar>
+      </div>
+    </div>
+  );
 };
 
 export default App;
