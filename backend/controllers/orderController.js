becabe3cb600f79f91f126b0f48d31816bce39bc
muhammad-amman <amman.soomro.as@gmail.com>
2024-12-29 23:51:51 +0500

    Create orderController.js

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,6 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import stripe from "stripe";
+
+// Place Order
+const placeOrder = async (req, res) => {};
