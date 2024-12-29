1e6664a8aeec53be8cbc63a42e340e461edc764e
muhammad-amman <amman.soomro.as@gmail.com>
2024-12-29 18:55:49 +0500

    Cart Router and Controller

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,9 @@
+import userModel from "../models/userModel";
+
+const addToCart = (req, res) => {};
+
+const removeFromCart = (req, res) => {};
+
+const getCart = (req, res) => {};
+
+export { addToCart, removeFromCart, getCart };
